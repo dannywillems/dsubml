827d2ef3476c75d824508437efdb3e306a11ae3a
Danny Willems <contact@danny-willems.be>
2017-03-02 19:46:35 +0100

    Improve the main file. WIP

--- src/main.ml
+++ src/main.ml
@@ -1,20 +1,98 @@
-let rec eval_file f =
+open Subtype
+
+(* ------------------------------------------------- *)
+(* About actions *)
+exception Undefined_action of string
+
+type action =
+  | Eval
+  | Subtype
+
+let action_of_string = function
+  | "eval" -> Eval
+  | "subtype" -> Subtype
+  | s -> raise (Undefined_action s)
+(* ------------------------------------------------- *)
+
+(* ------------------------------------------------- *)
+(* References for arguments *)
+let file_name = ref ""
+let eval_opt = ref ""
+(* ------------------------------------------------- *)
+
+(* ------------------------------------------------- *)
+let error_style = [ANSITerminal.red]
+let success_style = [ANSITerminal.green]
+(* ------------------------------------------------- *)
+
+(* ------------------------------------------------- *)
+(* Syntax error management *)
+let print_error lexbuf =
+  let pos = lexbuf.Lexing.lex_curr_p in
+  Printf.printf
+    "Syntax error %d:%d\n"
+    pos.Lexing.pos_lnum
+    (pos.Lexing.pos_cnum - pos.Lexing.pos_bol + 1)
+
+(* ------------------------------------------------- *)
+
+(* ------------------------------------------------- *)
+let print_is_subtype s t is_subtype =
+  ANSITerminal.printf
+    (if is_subtype then success_style else error_style)
+    "%s - %s is%s a subtype of %s\n"
+    (if is_subtype then "✓" else "❌")
+    (Print.string_of_raw_typ s)
+    (if is_subtype then "" else " not")
+    (Print.string_of_raw_typ t)
+
+let execute action lexbuf =
   try
-    let raw_term = Parser.top_level Lexer.prog f in
-    let nominal_term = Grammar.import_term AlphaLib.KitImport.empty raw_term in
-    print_endline "Raw term";
-    Print.raw_term raw_term;
-    print_endline "\nPrint nominal_term";
-    Print.raw_term @@ Grammar.show_term nominal_term;
-    print_endline "";
-    eval_file f
-  with End_of_file -> ()
+    action lexbuf
+  with
+  | End_of_file -> ()
+  | Parser.Error ->
+    print_error lexbuf;
+    exit 1
+
+let rec check_subtype f =
+  let (raw_s, raw_t) = Parser.top_level_subtype Lexer.prog f in
+  let nominal_s = Grammar.import_typ AlphaLib.KitImport.empty raw_s in
+  let nominal_t = Grammar.import_typ AlphaLib.KitImport.empty raw_t in
+  print_is_subtype raw_s raw_t (Subtype.subtype nominal_s nominal_t);
+  check_subtype f
+
+
+let rec eval_file f =
+  let raw_term = Parser.top_level Lexer.prog f in
+  let nominal_term = Grammar.import_term AlphaLib.KitImport.empty raw_term in
+  print_endline "Raw term";
+  Print.raw_term raw_term;
+  print_endline "\nPrint nominal_term";
+  Print.raw_term @@ Grammar.show_term nominal_term;
+  print_endline "";
+  eval_file f
+(* ------------------------------------------------- *)
+
+(* ------------------------------------------------- *)
+(* Args stuff *)
+let args_list = [
+  ("-f",
+   Arg.String (fun s -> file_name := s),
+   "File to read"
+  );
+  ("-a",
+   Arg.Symbol (["eval" ; "subtype"], (fun s -> eval_opt := s)),
+   "The action to do"
+  )
+]
 
 let () =
-  let argc = Array.length Sys.argv in
-  if argc > 1
-  then
-    let f = open_in (Array.get Sys.argv 1) in
-    (eval_file (Lexing.from_channel f); close_in f)
-  else
-    (print_endline "You must give a file"; exit 1)
+  Arg.parse args_list print_endline "An interpreter for DSub implemented in OCaml"
+(* ------------------------------------------------- *)
+
+let () =
+  let lexbuf = Lexing.from_channel (open_in (!file_name)) in
+  match (action_of_string (!eval_opt)) with
+  | Eval -> execute eval_file lexbuf
+  | Subtype -> execute check_subtype lexbuf
