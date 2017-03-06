b76a80b060fa8966ad8728d46007202378ff9107
Danny Willems <contact@danny-willems.be>
2017-03-06 15:41:00 +0100

    Add comments and a rule about the case x.A <: y.A

--- src/subtype.ml
+++ src/subtype.ml
@@ -1,8 +1,8 @@
-exception Not_a_type_declaration of Grammar.nominal_typ
+exception NotATypeDeclaration of Grammar.nominal_typ
 
 let tuple_of_type_declaration t = match t with
   | Grammar.TypeDeclaration(l, s, t) -> (l, s, t)
-  | _ -> raise (Not_a_type_declaration t)
+  | _ -> raise (NotATypeDeclaration t)
 
 let rec subtype_internal history context s t = match (s, t) with
   (* TOP *)
@@ -25,7 +25,17 @@ let rec subtype_internal history context s t = match (s, t) with
         t = t
     } in
     (DerivationTree.Node (node_value, history), true)
-  (* REFL. FIXME α-equality OK? Seems OK but must be more tested. *)
+  (* REFL. FIXME α-equality OK?
+     Seems OK but must be more tested.
+     By the way, is it very useful? Like TRANS, could it be implied by other
+     rules based on the types structure?
+     I don't think we can remove REFL because we can not recover the node x.A <:
+     x.A with another rule. The type x.A only appears in SEL-<: and <:-SEL. If
+     we want to have x.A <: x.A (or in a more general case x.A <: y.A), we need
+     to create a new rule comparing bounds (maybe using TYP <: TYP). This rule
+     must look after variables x and y in the environment.
+     See the « unofficial » REFL-TYP.
+  *)
   | (s, t) when (Grammar.equiv_typ s t) ->
     let node_value =
       DerivationTree.{
@@ -35,6 +45,43 @@ let rec subtype_internal history context s t = match (s, t) with
         t = t
     } in
     (DerivationTree.Node (node_value, history), true)
+    (* NOTE: It's not a defined subtyping rule. This rule is added to distinguish
+    the case Γ ⊦ x.A <: y.A. This case must not be handled by REFL because x.A
+    and y.A depends on the environment.
+
+    We call it REFL-TYP.
+
+    NOTE: As terms/types are not recursive, the corresponding hypothesis on the
+    variables must be removed from the environment. By the way, as the variable
+    is unique, it doesn't matter.
+    *)
+  | (Grammar.TypeProjection(x1, label_selected1),
+     Grammar.TypeProjection(x2, label_selected2)
+    ) ->
+    let label1, s1, t1 = tuple_of_type_declaration (ContextType.find x1 context) in
+    let label2, s2, t2 = tuple_of_type_declaration (ContextType.find x2 context) in
+    (* FIXME: We don't check that selected label are the same than label1 and
+    label2 *)
+    subtype_internal
+      history
+      context
+      (Grammar.TypeDeclaration(label1, s1, t1))
+      (Grammar.TypeDeclaration(label2, s2, t2))
+  (* TYP <: TYP *)
+  | Grammar.TypeDeclaration(tag1, s1, t1), Grammar.TypeDeclaration(tag2, s2, t2) ->
+    let node_value =
+      DerivationTree.{
+        rule = "TYP <: TYP";
+        env = context;
+        s = s;
+        t = t
+    } in
+    let left_derivation_tree, left_is_subtype = subtype_internal history context s2 s1 in
+    let right_derivation_tree, right_is_subtype = subtype_internal history context t1 t2 in
+    (
+      DerivationTree.Node (node_value, [left_derivation_tree ; right_derivation_tree]),
+      (tag1 == tag2) && left_is_subtype && right_is_subtype
+    )
   (* <: SEL *)
   | (s1, Grammar.TypeProjection(x, label_selected)) ->
     let node_value =
@@ -74,27 +121,20 @@ let rec subtype_internal history context s t = match (s, t) with
         s = s;
         t = t
     } in
-    let context' = ContextType.add x1 s2 (ContextType.add x2 s2 context) in
-    let left_derivation_tree, left_is_subtype = subtype_internal history context s2 s1 in
-    let right_derivation_tree, right_is_subtype = subtype_internal history context' t1 t2 in
+    let context' = ContextType.add x1 s1 (ContextType.add x2 s2 context) in
+    let left_derivation_tree, left_is_subtype =
+      subtype_internal history context s2 s1
+    in
+    let () = print_endline "right" in
+    let right_derivation_tree, right_is_subtype =
+      subtype_internal history context' t1 t2
+    in
     (
-      DerivationTree.Node (node_value, [left_derivation_tree ; right_derivation_tree]),
+      DerivationTree.Node (
+        node_value,
+        [left_derivation_tree ; right_derivation_tree]
+      ),
       left_is_subtype && right_is_subtype
-    )
-  (* TYP <: TYP *)
-  | Grammar.TypeDeclaration(tag1, s1, t1), Grammar.TypeDeclaration(tag2, s2, t2) ->
-    let node_value =
-      DerivationTree.{
-        rule = "TYP <: TYP";
-        env = context;
-        s = s;
-        t = t
-    } in
-    let left_derivation_tree, left_is_subtype = subtype_internal history context s2 s1 in
-    let right_derivation_tree, right_is_subtype = subtype_internal history context t1 t2 in
-    (
-      DerivationTree.Node (node_value, [left_derivation_tree ; right_derivation_tree]),
-      (tag1 == tag2) && left_is_subtype && right_is_subtype
     )
   | (_, _) -> DerivationTree.Empty, false
 
@@ -102,10 +142,15 @@ let subtype s t =
   subtype_internal [DerivationTree.Empty] (ContextType.empty ()) s t
 
 let type_of_internal context term = match term with
-| Grammar.TermTypeTag(type_tag, t) ->
-  Grammar.TypeDeclaration(type_tag, t, t)
-(* TODO *)
-| _ -> Grammar.TypeTop
+  (* TODO: VAR *)
+  (* TODO: SUB *)
+  (* TODO: LET *)
+  (* TODO: TYP-I *)
+  (* TODO: ALL-I *)
+  (* TODO: ALL-E *)
+  | Grammar.TermTypeTag(type_tag, t) ->
+    Grammar.TypeDeclaration(type_tag, t, t)
+  | _ -> Grammar.TypeTop
 
 let type_of term =
   type_of_internal (ContextType.empty ()) term
