addc9cae09c52ef1c94fdfc58c76aa7756be8e89
Danny Willems <contact@danny-willems.be>
2017-03-06 17:44:13 +0100

    tuple_of_dependent_function in TypeUtils.

--- src/typeUtils.ml
+++ src/typeUtils.ml
@@ -1,5 +1,11 @@
 exception NotATypeDeclaration of Grammar.nominal_typ
+exception NotADependentFunction of Grammar.nominal_typ
 
 let tuple_of_type_declaration t = match t with
   | Grammar.TypeDeclaration(l, s, t) -> (l, s, t)
   | _ -> raise (NotATypeDeclaration t)
+
+let tuple_of_dependent_function t = match t with
+  | Grammar.TypeDependentFunction(s, (x, t)) ->
+    (s, (x, t))
+  | _ -> raise (NotADependentFunction t)
--- src/typeUtils.mli
+++ src/typeUtils.mli
@@ -1,5 +1,10 @@
 exception NotATypeDeclaration of Grammar.nominal_typ
+exception NotADependentFunction of Grammar.nominal_typ
 
 val tuple_of_type_declaration :
   Grammar.nominal_typ ->
   (Grammar.type_tag * Grammar.nominal_typ * Grammar.nominal_typ)
+
+val tuple_of_dependent_function :
+  Grammar.nominal_typ ->
+  (Grammar.nominal_typ * (AlphaLib.Atom.atom * Grammar.nominal_typ))
