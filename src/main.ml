fd0b525f78cf5819bfed2e42c0c511a26f16e667
Danny Willems <contact@danny-willems.be>
2017-03-06 19:14:49 +0100

    Reorganize with the new actions like read_term and read_type.

--- src/main.ml
+++ src/main.ml
@@ -1,13 +1,19 @@
+open Eval
+
 (* ------------------------------------------------- *)
 (* About actions *)
 exception Undefined_action of string
 
 type action =
+  | Read_term
+  | Read_type
   | Eval
   | Subtype
   | Typing
 
 let action_of_string = function
+  | "read_term" -> Read_term
+  | "read_type" -> Read_type
   | "eval" -> Eval
   | "subtype" -> Subtype
   | "typing" -> Typing
@@ -72,6 +78,9 @@ let typing f =
     "%s\n"
     (Print.string_of_raw_typ (Grammar.show_typ type_of_t))
 
+let eval f =
+  ()
+
 let check_subtype f =
   let (raw_s, raw_t) = Parser.top_level_subtype Lexer.prog f in
   let nominal_s = Grammar.import_typ AlphaLib.KitImport.empty raw_s in
@@ -81,14 +90,23 @@ let check_subtype f =
   print_is_subtype raw_s raw_t is_subtype;
   print_endline "-------------------------"
 
-let eval_file f =
+let read_term_file f =
   let raw_term = Parser.top_level Lexer.prog f in
   let nominal_term = Grammar.import_term AlphaLib.KitImport.empty raw_term in
   print_endline "Raw term";
-  Print.raw_term raw_term;
+  Print.Style.raw_term [ANSITerminal.cyan] raw_term;
   print_endline "\nPrint nominal_term";
-  Print.raw_term @@ Grammar.show_term nominal_term;
-  print_endline ""
+  Print.Style.raw_term [ANSITerminal.blue] (Grammar.show_term nominal_term);
+  print_endline "\n-------------------------"
+
+let read_type_file f =
+  let raw_typ = Parser.top_level_type Lexer.prog f in
+  let nominal_typ = Grammar.import_typ AlphaLib.KitImport.empty raw_typ in
+  print_endline "Raw typ";
+  Print.Style.raw_typ [ANSITerminal.cyan] raw_typ;
+  print_endline "\nPrint nominal_typ";
+  Print.Style.raw_typ [ANSITerminal.blue] (Grammar.show_typ nominal_typ);
+  print_endline "\n-------------------------"
 (* ------------------------------------------------- *)
 
 (* ------------------------------------------------- *)
@@ -99,7 +117,7 @@ let args_list = [
    "File to read"
   );
   ("-a",
-   Arg.Symbol (["eval" ; "subtype" ; "typing"], (fun s -> eval_opt := s)),
+   Arg.Symbol (["read_term" ; "read_type" ; "subtype" ; "typing"], (fun s -> eval_opt := s)),
    "The action to do"
   );
   ("-v",
@@ -115,6 +133,8 @@ let () =
 let () =
   let lexbuf = Lexing.from_channel (open_in (!file_name)) in
   match (action_of_string (!eval_opt)) with
-  | Eval -> execute eval_file lexbuf
+  | Read_term -> execute read_term_file lexbuf
+  | Read_type -> execute read_type_file lexbuf
+  | Eval -> execute eval lexbuf
   | Subtype -> execute check_subtype lexbuf
   | Typing -> execute typing lexbuf
