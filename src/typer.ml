52ac8002a14286c1dcc66926964acdcbc28a1016
Danny Willems <contact@danny-willems.be>
2017-03-07 13:11:44 +0100

    Typer: add derivation tree in the algorithm.

--- src/typer.ml
+++ src/typer.ml
@@ -1,40 +1,124 @@
 exception TypeMismatch of string * (Grammar.nominal_typ * Grammar.nominal_typ)
-let rec type_of_internal context term = match term with
+exception NotTypable of Grammar.nominal_term
+
+let rec type_of_internal history context term = match term with
   (* ALL-I *)
   | Grammar.TermAbstraction(s_typ, (x, t)) ->
     let context' = ContextType.add x s_typ context in
-    let u = type_of_internal context' t in
-    Grammar.TypeDependentFunction(s_typ, (x, u))
+    let u_history, type_of_t = type_of_internal history context' t in
+    let typ = Grammar.TypeDependentFunction(s_typ, (x, type_of_t)) in
+    let typing_node = DerivationTree.{
+        rule = "ALL-I";
+        env = context;
+        term = term;
+        typ = typ
+      }
+    in (
+      DerivationTree.Node(
+        typing_node,
+        [u_history]
+      ),
+      typ
+    )
   (* TYP-I *)
   | Grammar.TermTypeTag(a, typ) ->
-    Grammar.TypeDeclaration(a, typ, typ)
+    let typ = Grammar.TypeDeclaration(a, typ, typ) in
+    let typing_node = DerivationTree.{
+        rule = "TYP-I";
+        env = context;
+        term = term ;
+        typ = typ ;
+      }
+    in (
+      DerivationTree.Node(
+        typing_node,
+        []
+      ),
+      typ
+    )
   (* LET *)
   | Grammar.TermLet(t, (x, u)) ->
-    let t_typ = type_of_internal context t in
+    let left_history, t_typ = type_of_internal history context t in
     (* It implies that x has the type of t, i.e. t_typ *)
     let x_typ = t_typ in
     let context' = ContextType.add x x_typ context in
-    let u_typ = type_of_internal context' u in
-    u_typ
+    let right_history, u_typ = type_of_internal history context' u in
+    let typing_node = DerivationTree.{
+        rule = "LET";
+        env = context;
+        term = term;
+        typ = u_typ
+      }
+    in (
+      DerivationTree.Node(
+        typing_node,
+        [left_history ; right_history]
+      ),
+      u_typ
+    )
   (* VAR *)
   | Grammar.TermVariable x ->
-    ContextType.find x context
+    let typ = ContextType.find x context in
+    let typing_node = DerivationTree.{
+        rule = "VAR";
+        env = context;
+        term = term;
+        typ = typ;
+      }
+    in (
+      DerivationTree.Node(
+        typing_node,
+        []
+      ),
+      typ
+    )
   (* ALL-E. TODO --> Need an idea to substitute. *)
   | Grammar.TermVarApplication(x, y) ->
     (* Hypothesis, get the corresponding types of x and y *)
-    let type_of_x = ContextType.find x context in
-    let type_of_y = ContextType.find y context in
+    (* We can simply use ContextType.find x context, but it's to avoid
+       duplicating code for the history construction
+    *)
+    let history_x, type_of_x =
+      type_of_internal history context (Grammar.TermVariable x)
+    in
+    let history_y, type_of_y =
+      type_of_internal history context (Grammar.TermVariable y)
+    in
     (* Check if x is a dependent function. *)
+    (* let x1 = s in t *)
     let (s, (x1, t)) = TypeUtils.tuple_of_dependent_function type_of_x in
-    if (Grammar.equiv_typ s type_of_y)
-    then t
+    if (Subtype.is_subtype ~context type_of_y s)
+    then (
+      let sigma = AlphaLib.Atom.Map.empty in
+      let sigma = AlphaLib.Atom.Map.add x1 s sigma in
+      let typ = AlphaLib.KitSubst.apply Grammar.copy_typ sigma t x1 in
+      let typing_node = DerivationTree.{
+          rule ="ALL-E";
+          env = context;
+          term = term;
+          typ = typ
+        }
+
+      in
+      let node = DerivationTree.Node(
+          typing_node,
+          [history_x ; history_y]
+        )
+      in
+      node, typ
+    )
     else raise
         (TypeMismatch (
-            "ALL-E: x must be of type dependent function", (s, type_of_y)
+            Printf.sprintf
+              "ALL-E: %s must be a subtype of %s but it's of type %s."
+              (AlphaLib.Atom.show y)
+              (Print.string_of_raw_typ (Grammar.show_typ s))
+              (Print.string_of_raw_typ (Grammar.show_typ type_of_y)),
+          (s, type_of_y)
           )
         )
   (* TODO: SUB *)
-  | _ -> Grammar.TypeTop
+  | _ -> raise (NotTypable term)
 
 let type_of term =
-  type_of_internal (ContextType.empty ()) term
+  type_of_internal [] (ContextType.empty ()) term
--- src/typer.mli
+++ src/typer.mli
@@ -1,1 +1,6 @@
-val type_of : Grammar.nominal_term -> Grammar.nominal_typ
+exception TypeMismatch of string * (Grammar.nominal_typ * Grammar.nominal_typ)
+exception NotTypable of Grammar.nominal_term
+
+val type_of :
+  Grammar.nominal_term ->
+  DerivationTree.typing_node DerivationTree.t * Grammar.nominal_typ
