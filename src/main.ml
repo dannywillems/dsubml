5690d0de1676cd92c53182b145aa72edfb8ef0d1
Danny Willems <contact@danny-willems.be>
2017-03-16 17:13:47 +0100

    Refactor using the TopLevelLet in typing algorithm. Extract print functions.

--- src/main.ml
+++ src/main.ml
@@ -51,6 +51,19 @@ let print_error lexbuf =
 
 (* ------------------------------------------------- *)
 (* Functions for actions *)
+
+(* The environment to convert raw terms/types to nominal terms/types.
+   Due to the design choice of the eval loop, we use a reference.
+*)
+let kit_import_env = ref AlphaLib.KitImport.empty
+
+(* The environment for typing and subtyping algorithms.
+   Due to the design choice of the eval loop, we use a reference.
+*)
+let typing_env = ref (ContextType.empty ())
+
+(* ---- Some printing functions ---- *)
+(** [print_is_subtyppe s t raw_is_subtype is_subtype] *)
 let print_is_subtype s t raw_is_subtype is_subtype =
   ANSITerminal.printf
     (if raw_is_subtype = is_subtype then success_style else error_style)
@@ -60,6 +73,37 @@ let print_is_subtype s t raw_is_subtype is_subtype =
     (if raw_is_subtype then "" else " not")
     (Print.string_of_raw_typ t)
 
+let print_raw_term_with_nominal_typ raw_term nominal_typ =
+  ANSITerminal.print_string
+    [ANSITerminal.cyan]
+    (Print.string_of_raw_term raw_term);
+  print_string " : ";
+  ANSITerminal.printf
+    [ANSITerminal.blue]
+    "%s\n"
+    (Print.string_of_nominal_typ nominal_typ);
+  print_endline "-------------------------"
+
+let print_derived_and_attended_types
+    derived_typ
+    attended_typ
+    nominal_term
+    same_type
+  =
+  ANSITerminal.printf
+    (if same_type then success_style else error_style)
+    "%s %s\n"
+    (if same_type then "✓" else "❌")
+    (Print.string_of_nominal_term nominal_term);
+  ANSITerminal.printf
+    [ANSITerminal.cyan]
+    "  Derived type: %s\n  Attending type: %s\n"
+    (Print.string_of_nominal_typ derived_typ)
+    (Print.string_of_nominal_typ attended_typ);
+  print_endline "-------------------------"
+
+
+(* The main loop to execute actions. *)
 let rec execute action lexbuf =
   try
     action lexbuf;
@@ -70,6 +114,8 @@ let rec execute action lexbuf =
     print_error lexbuf;
     exit 1
 
+(** Action to type check. *)
+(** [check_typing lexbuf] reads the next top level expression from [lexbuf] *)
 let check_typing f =
   let raw_term, raw_typ = Parser.top_level_check_typing Lexer.prog f in
   let nominal_term = Grammar.import_term AlphaLib.KitImport.empty raw_term in
@@ -77,33 +123,74 @@ let check_typing f =
   let history, derived_typ = Typer.type_of nominal_term in
   let same_type = Grammar.equiv_typ derived_typ nominal_typ in
   if !verbose then DerivationTree.print_typing_derivation_tree history;
-  ANSITerminal.printf
-    (if same_type then success_style else error_style)
-    "%s %s\n"
-    (if same_type then "✓" else "❌")
-    (Print.string_of_nominal_term nominal_term);
-  ANSITerminal.printf
-    [ANSITerminal.cyan]
-    "  Derived type: %s\n  Attending type: %s\n"
-    (Print.string_of_nominal_typ derived_typ)
-    (Print.string_of_nominal_typ nominal_typ);
-  print_endline "-------------------------"
+  print_derived_and_attended_types derived_typ nominal_typ nominal_term same_type
 
+(** Action to call the typechecker *)
 let typing f =
-  let raw_t = Parser.top_level Lexer.prog f in
-  let nominal_t = Grammar.import_term AlphaLib.KitImport.empty raw_t in
-  let history, type_of_t = Typer.type_of nominal_t in
-  if !verbose then DerivationTree.print_typing_derivation_tree history;
-  ANSITerminal.print_string
-    [ANSITerminal.cyan]
-    (Print.string_of_raw_term raw_t);
-  print_string " : ";
-  ANSITerminal.printf
-    [ANSITerminal.blue]
-    "%s\n"
-    (Print.string_of_nominal_typ type_of_t);
-  print_endline "-------------------------"
+  match Parser.top_level Lexer.prog f with
+  | Grammar.Term raw_t ->
+    let nominal_t =
+      Grammar.import_term
+        (!kit_import_env)
+        raw_t
+    in
+    let history, type_of_t =
+      Typer.type_of
+        ~context:(!typing_env)
+        nominal_t
+    in
+    if !verbose
+    then DerivationTree.print_typing_derivation_tree history;
+    print_raw_term_with_nominal_typ raw_t type_of_t
+    (* let x = t. Top level expressions. Can not appear in other expressions. *)
+  | Grammar.TopLevelLet (x, raw_typ, raw_t) ->
+    (* Convert raw term/type to nominal term/type using the import environment. *)
+    let nominal_t =
+      Grammar.import_term
+        (!kit_import_env)
+        raw_t
+    in
+    let nominal_typ =
+      Grammar.import_typ
+        (!kit_import_env)
+        raw_typ
+    in
+    (* Infer the type of t *)
+    let history, type_of_t =
+      Typer.type_of
+        ~context:(!typing_env)
+        nominal_t
+    in
+    let extended_kit_import_env, atom_x =
+      AlphaLib.KitImport.extend
+        (!kit_import_env)
+        x
+    in
+    (* The inferred type must be a subtype of the wanted type. *)
+    if Subtype.is_subtype type_of_t nominal_typ
+    then (
+      (* If verbose is activated, we print the typing derivation tree *)
+      if !verbose
+      then DerivationTree.print_typing_derivation_tree history;
+      Printf.printf "(Top level let definition)\n";
+      print_raw_term_with_nominal_typ raw_t type_of_t;
+      kit_import_env := extended_kit_import_env;
+      typing_env := ContextType.add atom_x nominal_typ (!typing_env)
+    )
+    else
+      raise (
+        Error.SubtypeError (
+          (Printf.sprintf
+             "%s is not a subtype of %s"
+             (Print.string_of_nominal_typ type_of_t)
+             (Print.string_of_raw_typ raw_typ)
+          ),
+          type_of_t,
+          nominal_typ
+        )
+      )
 
+(** Action to check all algorithms for subtyping give the same results. *)
 let check_subtype_algorithms f =
   let (raw_is_subtype, raw_s, raw_t) = Parser.top_level_subtype Lexer.prog f in
   let nominal_s = Grammar.import_typ AlphaLib.KitImport.empty raw_s in
@@ -130,9 +217,16 @@ let check_subtype_algorithms f =
     "With REFL";
   print_endline "-------------------------"
 
+(** Action to evaluate a file.
+    NOTE: We can erase the types because we don't need it when evaluating.
+*)
 let eval f =
   ()
 
+(** Action to check the subtype algorithm (with or without REFL). It uses the
+    syntax S <: T or S !<: T and automatically check if the answer is the same than
+    we want.
+*)
 let check_subtype ~with_refl f =
   let (raw_is_subtype, raw_s, raw_t) = Parser.top_level_subtype Lexer.prog f in
   let nominal_s = Grammar.import_typ AlphaLib.KitImport.empty raw_s in
@@ -142,15 +236,19 @@ let check_subtype ~with_refl f =
   print_is_subtype raw_s raw_t raw_is_subtype is_subtype;
   print_endline "-------------------------"
 
+(** Action to read a file with list of terms. *)
 let read_term_file f =
-  let raw_term = Parser.top_level Lexer.prog f in
-  let nominal_term = Grammar.import_term AlphaLib.KitImport.empty raw_term in
-  print_endline "Raw term";
-  Print.Style.raw_term [ANSITerminal.cyan] raw_term;
-  print_endline "\nPrint nominal_term";
-  Print.Style.nominal_term [ANSITerminal.blue] nominal_term;
-  print_endline "\n-------------------------"
+  match Parser.top_level Lexer.prog f with
+  | Grammar.Term raw_term ->
+    let nominal_term = Grammar.import_term (!kit_import_env) raw_term in
+    print_endline "Raw term";
+    Print.Style.raw_term [ANSITerminal.cyan] raw_term;
+    print_endline "\nPrint nominal_term";
+    Print.Style.nominal_term [ANSITerminal.blue] nominal_term;
+    print_endline "\n-------------------------"
+  | Grammar.TopLevelLet (x, typ, term) -> () (* TODO *)
 
+(** Action to read a file with list of types. *)
 let read_type_file f =
   let raw_typ = Parser.top_level_type Lexer.prog f in
   let nominal_typ = Grammar.import_typ AlphaLib.KitImport.empty raw_typ in
