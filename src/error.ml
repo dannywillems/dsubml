ccddec82fdb0663d724ce1d02f00a934a13874ee
Danny Willems <contact@danny-willems.be>
2017-03-18 17:29:21 +0100

    Avoidance problem error: add a string.

--- src/error.ml
+++ src/error.ml
@@ -1,3 +1,3 @@
 exception SubtypeError of string * Grammar.nominal_typ * Grammar.nominal_typ
 
-exception AvoidanceProblem of AlphaLib.Atom.t * Grammar.nominal_typ
+exception AvoidanceProblem of string * AlphaLib.Atom.t * Grammar.nominal_typ
