aef344a1d8eebc444e8619f2407851f9305dfd4c
Danny Willems <contact@danny-willems.be>
2017-03-28 15:58:42 +0200

    Use check functions instead of raising exception in the typing algorithm.

--- src/typer.ml
+++ src/typer.ml
@@ -1,64 +1,37 @@
-let check_well_formedness context typ =
-  if not (WellFormed.typ context typ)
-  then raise (Error.NotWellFormed(context, typ))
-
 let rec type_of_internal history context term = match term with
   (* ALL-I
-     Γ, x : S ⊦ t : U ∧ x \notin FV(S) =>
-     Γ ⊦ λ(x : S) t ⊦ ∀(x : S) U'
+     Γ, x : S ⊦ t : U ∧ x ∉ FV(S)
+     =>
+     Γ ⊦ λ(x : S) t ⊦ ∀(x : S) U
   *)
   | Grammar.TermAbstraction(s, (x, t)) ->
-    check_well_formedness context s;
+    Error.check_well_formedness context s;
+    Error.check_avoidance_problem x s;
     let context' = ContextType.add x s context in
     let u_history, u = type_of_internal history context' t in
     let typ = Grammar.TypeDependentFunction(s, (x, u)) in
-    let typing_node = DerivationTree.{
-        rule = "ALL-I";
-        env = context;
-        term = term;
-        typ = typ
-      }
-    in
-    if Grammar.occurs_typ x s
-    then raise (Error.AvoidanceProblem(
-        (Printf.sprintf
-           "%s appears in %s."
-           (AlphaLib.Atom.show x)
-           (Print.string_of_nominal_typ s)
-        ),
-        x,
-        s
-      ))
-    else (
-      DerivationTree.Node(
-        typing_node,
-        [u_history]
-      ),
-      typ
-    )
+    DerivationTree.create_typing_node
+      ~rule:"ALL-I"
+      ~env:context
+      ~term
+      ~typ
+      ~history:[u_history]
   (* TYP-I
      Γ ⊦ { A = T } : { A : T .. T }
   *)
   | Grammar.TermTypeTag(a, typ) ->
-    check_well_formedness context typ;
+    Error.check_well_formedness context typ;
     let typ = Grammar.TypeDeclaration(a, typ, typ) in
-    let typing_node = DerivationTree.{
-        rule = "TYP-I";
-        env = context;
-        term = term ;
-        typ = typ ;
-      }
-    in (
-      DerivationTree.Node(
-        typing_node,
-        []
-      ),
-      typ
-    )
+     DerivationTree.create_typing_node
+      ~rule:"TYP-I"
+      ~env:context
+      ~term
+      ~typ
+      ~history:[]
   (* LET
      Γ ⊦ t : T ∧
      Γ, x : T ⊦ u : U ∧
-     x \notin FV(U)
+     x ∉ FV(U)
      =>
      Γ ⊦ let x = t in u : U
   *)
@@ -68,48 +41,24 @@ let rec type_of_internal history context term = match term with
     let x_typ = t_typ in
     let context' = ContextType.add x x_typ context in
     let right_history, u_typ = type_of_internal history context' u in
-    let typing_node = DerivationTree.{
-        rule = "LET";
-        env = context;
-        term = term;
-        typ = u_typ
-      }
-    in
-    if Grammar.occurs_typ x u_typ
-    then raise (Error.AvoidanceProblem(
-        (Printf.sprintf
-           "%s appears in %s."
-           (AlphaLib.Atom.show x)
-           (Print.string_of_nominal_typ u_typ)
-        ),
-        x,
-        u_typ
-      ))
-    else (
-      DerivationTree.Node(
-        typing_node,
-        [left_history ; right_history]
-      ),
-      u_typ
-    )
+    Error.check_avoidance_problem x u_typ;
+    DerivationTree.create_typing_node
+      ~rule:"LET"
+      ~env:context
+      ~term
+      ~typ:u_typ
+      ~history:[left_history ; right_history]
   (* VAR
      Γ, x : T, Γ' ⊦ x : T
   *)
   | Grammar.TermVariable x ->
     let typ = ContextType.find x context in
-    let typing_node = DerivationTree.{
-        rule = "VAR";
-        env = context;
-        term = term;
-        typ = typ;
-      }
-    in (
-      DerivationTree.Node(
-        typing_node,
-        []
-      ),
-      typ
-    )
+    DerivationTree.create_typing_node
+      ~rule:"VAR"
+      ~env:context
+      ~term
+      ~typ
+      ~history:[]
   (* ALL-E.
      Γ ⊦ x : ∀(z : S) : T ∧
      Γ ⊦ y : S
@@ -129,85 +78,45 @@ let rec type_of_internal history context term = match term with
     in
     (* Check if [x] is a dependent function. *)
     let (s, (z, t)) = TypeUtils.tuple_of_dependent_function type_of_x in
-    let _, is_subtype = Subtype.subtype ~context type_of_y s in
-    if is_subtype
-    then (
-      (* Here, we rename the variable [x1] (which is the variable in the for all
-         type, by the given variable [y]). We don't substitute the variable by
-         the right types because it doesn't work with not well formed types (like
-         x.A when x is of types Any).
-      *)
-      let typ = Grammar.rename_typ (AlphaLib.Atom.Map.singleton z y) t in
-      let typing_node = DerivationTree.{
-          rule ="ALL-E";
-          env = context;
-          term = term;
-          typ = typ
-        }
-
-      in
-      let node = DerivationTree.Node(
-          typing_node,
-          [history_x ; history_y]
-        )
-      in
-      node, typ
-    )
-    else raise
-        (Error.TypeMismatch (
-            Printf.sprintf
-              "ALL-E: %s must be a subtype of %s but it's of type %s."
-              (AlphaLib.Atom.show y)
-              (Print.string_of_raw_typ (Grammar.show_typ s))
-              (Print.string_of_raw_typ (Grammar.show_typ type_of_y)),
-          (s, type_of_y)
-          )
-        )
+    Error.check_type_match context (Grammar.TermVariable y) type_of_y s;
+    (* Here, we rename the variable [x1] (which is the variable in the for all
+       type, by the given variable [y]). We don't substitute the variable by
+       the right types because it doesn't work with not well formed types (like
+       x.A when x is of types Any).
+    *)
+    let typ = Grammar.rename_typ (AlphaLib.Atom.Map.singleton z y) t in
+    DerivationTree.create_typing_node
+      ~rule:"ALL-E"
+      ~env:context
+      ~term
+      ~typ
+      ~history:[history_x ; history_y]
   (* ----- Unofficial typing rules ----- *)
   (* UN-ASC
      Γ ⊦ t : T
   *)
   | Grammar.TermAscription(t, typ_of_t) ->
-    check_well_formedness context typ_of_t;
     let actual_history, actual_typ_of_t =
       type_of_internal history context t
     in
-    if Subtype.is_subtype ~context actual_typ_of_t typ_of_t
-    then (
-      let typing_node = DerivationTree.{
-          rule = "UN-ASC";
-          env = context;
-          term = term;
-          typ = typ_of_t;
-        }
-      in
-      let node = DerivationTree.Node(
-          typing_node,
-          [actual_history]
-        )
-      in
-      node, typ_of_t
-    )
-    else (
-      raise (Error.SubtypeError ("TODO", actual_typ_of_t, typ_of_t))
-    )
+    Error.check_well_formedness context typ_of_t;
+    Error.check_subtype context actual_typ_of_t typ_of_t;
+    DerivationTree.create_typing_node
+      ~rule:"UN-ASC"
+      ~env:context
+      ~term
+      ~typ:typ_of_t
+      ~history:[actual_history]
   (* UN-UNIMPLEMENTED
      Γ ⊦ Unimplemented : ⟂
   *)
   | Grammar.TermUnimplemented ->
-    let typing_node = DerivationTree.{
-        rule = "UN-UNIMPLEMENTED";
-        env = context;
-        term = term;
-        typ = Grammar.TypeBottom;
-      }
-    in
-    let node = DerivationTree.Node(
-        typing_node,
-        history
-      )
-    in
-    node, Grammar.TypeBottom
+    DerivationTree.create_typing_node
+      ~rule:"UN-UNIMPLEMENTED"
+      ~env:context
+      ~term
+      ~typ:Grammar.TypeBottom
+      ~history
 
 let type_of ?(context = ContextType.empty ()) term =
   type_of_internal [] context term
